c2269378ea79f9a7dc509f978c7a085ae6782205
Aashish Wagle <meaashiswagle@gmail.com>
2019-02-18 15:03:41 +0545

    hello toast potrait and landscape completed

--- week1/HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ week1/HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -10,12 +10,14 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private TextView land_showCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.dis_count);
+        land_showCount = (TextView) findViewById(R.id.disCount_land);
     }
 
     public void showToast(View view) {
@@ -27,6 +29,18 @@ public class MainActivity extends AppCompatActivity {
         ++mCount;
         if(mShowCount != null){
             mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void showToastL(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUpL(View view) {
+        ++mCount;
+        if(land_showCount != null){
+            land_showCount.setText(Integer.toString(mCount));
         }
     }
 }
