e447c2b134b8f03d7918181f2115e9644f8f943f
Aashish Wagle <meaashiswagle@gmail.com>
2019-02-19 13:04:42 +0545

    hello toast homework done, reset text part!

---
+++ week1/HelloToastHomework/app/src/main/java/com/example/helloToastHomework/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.helloToastHomework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    private TextView land_showCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.dis_count);
+        land_showCount = (TextView) findViewById(R.id.disCount_land);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void showToastL(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUpL(View view) {
+        ++mCount;
+        if(land_showCount != null){
+            land_showCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void resetText(View view) {
+        if(mShowCount != null){
+           mShowCount.setText("0");
+        }
+    }
+
+    public void resetText_land(View view) {
+        if(land_showCount != null){
+            land_showCount.setText("0");
+        }
+    }
+}
