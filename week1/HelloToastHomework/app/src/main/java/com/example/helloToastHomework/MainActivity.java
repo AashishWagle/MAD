d4075b46bfa62b203b0a7a96bc421a3ea8ac8801
Aashish Wagle <meaashiswagle@gmail.com>
2019-02-21 13:28:30 +0545

    Homework of week 1 - part 1

--- week1/HelloToastHomework/app/src/main/java/com/example/helloToastHomework/MainActivity.java
+++ week1/HelloToastHomework/app/src/main/java/com/example/helloToastHomework/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloToastHomework;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
     private TextView land_showCount;
+    private Button btnReset;
+    private Button btnResetL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.dis_count);
         land_showCount = (TextView) findViewById(R.id.disCount_land);
+        btnReset = (Button) findViewById(R.id.btnReset);
+        btnResetL = (Button) findViewById(R.id.button);
     }
 
     public void showToast(View view) {
@@ -29,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         ++mCount;
         if(mShowCount != null){
             mShowCount.setText(Integer.toString(mCount));
+            if(mCount % 2 == 0){
+                btnReset.setBackgroundColor(getResources().getColor(R.color.evenColor));
+            }else{
+                btnReset.setBackgroundColor(getResources().getColor(R.color.oddColor));
+            }
         }
     }
 
@@ -41,18 +51,27 @@ public class MainActivity extends AppCompatActivity {
         ++mCount;
         if(land_showCount != null){
             land_showCount.setText(Integer.toString(mCount));
+            if(mCount % 2 == 0){
+                btnResetL.setBackgroundColor(getResources().getColor(R.color.evenColor));
+            }else{
+                btnResetL.setBackgroundColor(getResources().getColor(R.color.oddColor));
+            }
         }
     }
 
     public void resetText(View view) {
         if(mShowCount != null){
+            mCount = 0;
            mShowCount.setText("0");
+            btnReset.setBackgroundColor(getResources().getColor(R.color.evenColor));
         }
     }
 
     public void resetText_land(View view) {
         if(land_showCount != null){
+            mCount = 0;
             land_showCount.setText("0");
+            btnResetL.setBackgroundColor(getResources().getColor(R.color.evenColor));
         }
     }
 }
